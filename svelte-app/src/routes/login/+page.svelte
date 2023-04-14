d11e17b41c3a03e802edb5d295ea239e5274acb7
Markus Thielker <mail.markus.thielker@gmail.com>
2023-04-14 20:52:24 +0200

    feat(auth): add login page

---
+++ svelte-app/src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {enhance} from "$app/forms"
+</script>
+
+<div class="flex items-center justify-center">
+
+    <div class="card flex flex-col max-w-md w-full space-y-4 ">
+
+        <h1 class="text-xl text-center font-semibold">Admin Login</h1>
+
+        <form class="flex flex-col space-y-2" method="POST" use:enhance>
+            <div class="flex flex-col">
+                <label for="username">Username</label>
+                <input class="input-text" id="username" name="username"/>
+            </div>
+            <div class="flex flex-col">
+                <label for="password">Password</label>
+                <input class="input-text" id="password" name="password" type="password"/>
+            </div>
+            <input class="btn-primary" type="submit" value="Login"/>
+        </form>
+    </div>
+
+</div>
