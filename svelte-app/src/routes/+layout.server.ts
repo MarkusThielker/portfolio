e37b7b2e0cefbe6e875b6e2d58d5ab4b58171e67
Markus Thielker <mail.markus.thielker@gmail.com>
2023-04-15 14:24:26 +0200

    feat(auth): add authentication hook

---
+++ svelte-app/src/routes/+layout.server.ts
@@ -0,0 +1,6 @@
+import type {LayoutServerLoad} from "./$types"
+
+/** @type {import("./$types").LayoutServerLoad} */
+export const load: LayoutServerLoad = async ({locals}) => {
+    return await locals.validateUser()
+}
