3ef904584735611d172e66eb05bd8dedffd0c20f
Markus Thielker <mail.markus.thielker@gmail.com>
2023-04-20 21:36:24 +0200

    feat(auth): add notifications on logout

--- svelte-app/src/routes/+page.server.ts
+++ svelte-app/src/routes/+page.server.ts
@@ -1,11 +1,26 @@
-import type {PageServerLoad} from "./$types"
+import type {Actions, PageServerLoad} from "./$types"
 import {prismaClient} from "$lib/server/prisma"
 import type {Post} from "@prisma/client"
+import {auth} from "$lib/server/lucia"
+import {NotificationType} from "$lib/notification"
 
 /** @type {import("./$types").PageServerLoad} */
-export const load = (async () => {
+export const load = (async ({locals}) => {
 
-    let posts: Post[] = await prismaClient.post.findMany({where: {published: true}})
-    return {posts: posts}
+    const {user} = await locals.validateUser()
+    const posts: Post[] = await prismaClient.post.findMany({where: {published: true}})
+    return {posts: posts, user: user}
 
 }) satisfies PageServerLoad
+
+export const actions: Actions = {
+    logout: async ({request, locals}) => {
+
+        const {session} = await locals.validateUser()
+        if (session) {
+            await auth.invalidateSession(session.sessionId)
+            return {notification: {type: NotificationType.SUCCESS, message: "Logged out successfully"}}
+        }
+        return {notification: {type: NotificationType.ERROR, message: "Something unexpected happened"}}
+    },
+}
--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -4,13 +4,35 @@
     import Navigation from "$lib/components/Navigation.svelte"
     import PostPreview from "$lib/components/PostPreview.svelte"
     import PageTransition from "$lib/components/PageTransition.svelte"
+    import {notifications} from "$lib/notification"
 
     /** @type {import("./$types").PageData} */
     export let data
 
+    /** @type {import("./$types").ActionData} */
+    export let form
+    $: {
+        if (form) notifications.show(form.notification.type, form.notification.message)
+    }
+
+    $: isAuthenticated = data.user
+
 </script>
 
 <PageTransition>
+
+    {#if isAuthenticated}
+        <form method="POST" action="?/logout" class="flex justify-end">
+            <button type="submit" class="inline-flex space-x-2 btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"/>
+                </svg>
+                <span class="pr-1">Logout</span>
+            </button>
+        </form>
+    {/if}
 
     <Introduction/>
 
