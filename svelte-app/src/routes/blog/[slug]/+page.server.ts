9a4a819699a25bc98f9ce29b7d21fb8f83eb60e9
Markus Thielker <mail.markus.thielker@gmail.com>
2023-04-22 13:49:49 +0200

    feat(blog): add button to publish/unpublish a post

--- svelte-app/src/routes/blog/[slug]/+page.server.ts
+++ svelte-app/src/routes/blog/[slug]/+page.server.ts
@@ -1,8 +1,9 @@
-import type {PageServerLoad} from "./$types"
+import type {Actions, PageServerLoad} from "./$types"
 import {prismaClient} from "$lib/server/prisma"
 import type {Post} from "@prisma/client"
 import {redirect} from "@sveltejs/kit"
 import {showdownConverter} from "$lib/showdown"
+import {NotificationType} from "$lib/notification"
 
 /** @type {import("./$types").PageServerLoad} */
 export const load = (async ({locals, params}) => {
@@ -26,6 +27,59 @@ export const load = (async ({locals, params}) => {
         },
     })
     post.content = showdownConverter.makeHtml(post.content)
-    return {post: post}
+
+    return {post: post, session: session}
 
 }) satisfies PageServerLoad
+
+export const actions: Actions = {
+    publish: async ({request, locals}) => {
+
+        const {session} = await locals.validateUser()
+
+        if (!session) return {notification: {type: NotificationType.ERROR, message: "No permission to publish post"}}
+
+        const url = request.url.replaceAll("?", "").split("/")
+        const slug = url[url.length - 2]
+
+        try {
+            await prismaClient.post.update({
+                where: {slug: slug},
+                data: {
+                    published: true,
+                    publishedAt: new Date(),
+                },
+            })
+            return {notification: {type: NotificationType.SUCCESS, message: "Post published successfully"}}
+
+        } catch (e) {
+            return {notification: {type: NotificationType.ERROR, message: "Something unexpected happened"}}
+        }
+    },
+    unpublish: async ({request, locals}) => {
+
+        const {session} = await locals.validateUser()
+
+        if (!session) return {notification: {type: NotificationType.ERROR, message: "No permission to unpublish post"}}
+
+        const url = request.url.replaceAll("?", "").split("/")
+        const slug = url[url.length - 2]
+
+        try {
+            await prismaClient.post.update({
+                where: {slug: slug},
+                data: {
+                    published: false,
+                },
+            })
+            return {notification: {type: NotificationType.SUCCESS, message: "Post unpublished successfully"}}
+
+        } catch (e) {
+            return {notification: {type: NotificationType.ERROR, message: "Something unexpected happened"}}
+        }
+    },
+    delete: async ({request, locals}) => {
+
+    },
+}
+
--- svelte-app/src/routes/blog/[slug]/+page.svelte
+++ svelte-app/src/routes/blog/[slug]/+page.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
 
+    import {enhance} from "$app/forms"
     import PageTransition from "$lib/components/PageTransition.svelte"
+    import {notifications} from "$lib/notification"
 
     /** @type {import("./$types").PageData} */
     export let data
 
+    $: isAuthenticated = data.session
+
+    /** @type {import("./$types").FormData} */
+    export let form
+    $: {
+        if (form) notifications.show(form.notification.type, form.notification.message)
+    }
+
 </script>
 
 <PageTransition>
+
+    <div class="flex flex-col space-y-3">
+
+        <nav class="flex px-4 py-2.5 rounded-2xl bg-stone-700/70 backdrop-blur">
+
+            <form action="{data.post.published ? '?/unpublish' : '?/publish'}" method="POST" use:enhance>
+                <button class="btn btn-primary" type="submit">
+                    {data.post.published ? 'Unpublish' : 'Publish'}
+                </button>
+            </form>
+
+        </nav>
+
+    </div>
 
     <div class="flex flex-col space-y-3">
 
