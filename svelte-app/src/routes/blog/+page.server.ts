9533dc9e96afcf48d3e794728e49aea87406ec11
Markus Thielker <mail.markus.thielker@gmail.com>
2023-04-22 14:55:06 +0200

    feat(blog): add button to create a post

--- svelte-app/src/routes/blog/+page.server.ts
+++ svelte-app/src/routes/blog/+page.server.ts
@@ -1,6 +1,7 @@
-import type {PageServerLoad} from "./$types"
+import type {Actions, PageServerLoad} from "./$types"
 import {prismaClient} from "$lib/server/prisma"
 import type {Post} from "@prisma/client"
+import {NotificationType} from "$lib/notification"
 
 /** @type {import("./$types").PageServerLoad} */
 export const load = (async ({locals}) => {
@@ -9,6 +10,31 @@ export const load = (async ({locals}) => {
 
     let posts: Post[] | null = await prismaClient.post.findMany(session ? undefined : {where: {published: true}})
 
-    return {posts: posts}
+    return {posts: posts, session: session}
 
 }) satisfies PageServerLoad
+
+export const actions: Actions = {
+    create: async ({request, locals}) => {
+
+        const {session} = await locals.validateUser()
+
+        if (!session) return {notification: {type: NotificationType.ERROR, message: "No permission to create draft"}}
+
+        const slug = "new-post-" + new Date().getTime()
+        try {
+            await prismaClient.post.create({
+                data: {
+                    slug: slug,
+                    title: "New Post",
+                    content: "Start your post here...",
+                    published: false,
+                },
+            })
+            return {notification: {type: NotificationType.SUCCESS, message: "Draft created successfully"}}
+
+        } catch (e) {
+            return {notification: {type: NotificationType.ERROR, message: "Something unexpected happened"}}
+        }
+    },
+}
--- svelte-app/src/routes/blog/+page.svelte
+++ svelte-app/src/routes/blog/+page.svelte
@@ -2,13 +2,39 @@
 
     import PostPreview from "$lib/components/PostPreview.svelte"
     import PageTransition from "$lib/components/PageTransition.svelte"
+    import {notifications} from "$lib/notification"
 
     /** @type {import("./$types").PageData} */
     export let data
 
+    $: isAuthenticated = data.session
+
+    /** @type {import("./$types").FormData} */
+    export let form
+    $: {
+        if (form) notifications.show(form.notification.type, form.notification.message)
+    }
+
 </script>
 
 <PageTransition>
+
+    {#if isAuthenticated}
+
+        <div class="fixed bottom-6 right-6 sm:bottom-12 sm:right-12">
+
+            <form method="POST" action="?/create">
+                <button type="submit" class="fab-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                    </svg>
+                </button>
+            </form>
+
+        </div>
+
+    {/if}
 
     <h1>Blog</h1>
 
