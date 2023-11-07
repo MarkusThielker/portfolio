d44d80561cbb6725f291835c11fe51eaeaafd720
Markus Thielker <mail.markus.thielker@gmail.com>
2023-11-07 23:48:42 +0100

    PORTFOLIO-11: add CSS style for inputs

--- src/lib/components/PlainTextEditor.svelte
+++ src/lib/components/PlainTextEditor.svelte
@@ -7,5 +7,5 @@
 
 <input 
     bind:value={content} 
-    class="w-full p-0 bg-transparent ring-0 focus:ring-0 border-0 placeholder:text-gray-400 {textType}"
+    class="unstyled-input {textType}"
     type="text"/>
