b10f7a851c59cc6561cbdb617ab0088c165ad60c
Markus Thielker <mail.markus.thielker@gmail.com>
2023-11-09 00:05:17 +0100

    PORTFOLIO-3: add hidden inputs and replace icons

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,7 +3,6 @@
     import { fade } from "svelte/transition";
     import PageTransition from "$lib/components/PageTransition.svelte";
     import { notifications, NotificationType } from "$lib/notification";
-    import PlainText from "$lib/components/PlainText.svelte";
     import PlainTextArea from "$lib/components/TextArea.svelte";
     import type { PageData } from "./$types";
 
@@ -47,12 +46,10 @@
     let isEditable = false;
 </script>
 
-
 <svelte:head>
     <title>{data.post.title} - Markus Thielker</title>
-    <meta property="description" content={data.post.teaser}/>
+    <meta property="description" content={data.post.teaser} />
 </svelte:head>
-
 
 <PageTransition>
     {#if isAuthenticated}
@@ -65,45 +62,15 @@
                     method="POST"
                     use:enhance
                 >
+                    <input type="hidden" name="slug" value={data.post.slug} />
                     <button
                         class="flex space-x-1 btn-primary h-8 items-center justify-center"
                         type="submit"
                     >
                         {#if data.post.published}
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                                />
-                            </svg>
+                            <span class="material-icons-round">visibility</span>
                         {:else}
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                                />
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                />
-                            </svg>
+                            <span class="material-icons-round">visibility_off</span>
                         {/if}
                         <span
                             >{data.post.published
@@ -126,20 +93,7 @@
                             initOrReset();
                         }}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
+                        <span class="material-icons-round">close</span>
                         <span>Cancel</span>
                     </button>
                     <form method="POST" action="?/save" use:enhance>
@@ -173,20 +127,7 @@
                             class="flex btn-primary space-x-1 h-8 items-center justify-center"
                             type="submit"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M4.5 12.75l6 6 9-13.5"
-                                />
-                            </svg>
+                            <span class="material-icons-round">check</span>
                             <span>Save</span>
                         </button>
                     </form>
@@ -196,20 +137,7 @@
                         type="button"
                         on:click={() => (isEditable = true)}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                            />
-                        </svg>
+                        <span class="material-icons-round">edit</span>
                         <span>Edit</span>
                     </button>
                 {/if}
@@ -219,6 +147,7 @@
                     method="POST"
                     use:enhance
                 >
+                    <input type="hidden" name="slug" value={data.post.slug} />
                     <button
                         class="flex space-x-1 btn-primary h-8 items-center justify-center"
                         type="submit"
@@ -238,20 +167,7 @@
                     type="button"
                     on:click={() => (deleteConfirmationVisible = true)}
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                        />
-                    </svg>
+                    <span class="material-icons-round">delete</span>
                     <span>Delete</span>
                 </button>
             </nav>
